4dee18aa0fbbed6de99e79b177f3077dc97df82c
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2024-07-05 19:53:01 +0300

    Update x-store.js

--- x-store.js
+++ x-store.js
@@ -25,3 +25,29 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 2300)
     })
 })
+// Splash screen
+let introPage = document.querySelector('.intro');
+let logoPage = document.querySelector('.logo-header');
+let logospanPage = document.querySelectorAll('.logo-intro');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logospanPage.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+        setTimeout(() => {
+            logospanPage.forEach((span, idx) =>{
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 2000)
+        
+        setTimeout(() => {
+            introPage.style.top = '-100vh';
+        }, 2300)
+    })
+})
